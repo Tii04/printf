a55d279c1cb302d4ca78a8a1063e9aaa163d7260
Tiisetso <tiisetso.qsebata@gmail.com>
2022-08-08 13:01:00 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -17,24 +17,34 @@ int _printf(const char *format, ...)
 
 	va_start(data, format);
 
-	for (i = 0; format[i] != '\0';)
+	for (int i = 0; format[i] != '\0';)
 	{
 		if (format[i] != '%')
-		{
-			count += _putchar(format[i])
-		}
+			count += _putchar(format[i]);
+
 		i++;
-		else if (format[i] == '%' && format[i + 1] != ' ')
+
+		if (format[i] == '%' && format[i + 1] != ' ')
 		{
 			switch (format[i + 1])
 			{
-				case 'c': count += _putchar(va_arg(data, int));
+				case 'c':
+					
+					count += _putchar(va_arg(data, int));
+
 					  break;
 
-				case 's': count += print_string(va_arg(data, char *));
+				case 's': 
+					  
+					  count += print_string(va_arg(data, char *));
+
 					  break;
 
-				case '%': count += _putchar('%');
+				case '%': 
+					  
+					  count += _putchar('%');
+
+					  break;
 			}
 		}
 	}
@@ -44,7 +54,12 @@ int _printf(const char *format, ...)
 
 }
 
-
+/**
+ * print_string - Prints string.
+ * @string: pointer.
+ * Return: int.
+ *
+ */
 int print_string(char *string)
 {
 	int count = 0;

821b87897deeaa07b150c339fdabe18f929ab596
lola10209 <shanteltestimony@gmail.com>
2022-08-09 02:13:44 -0700

    Current directory

--- printf.c
+++ printf.c
@@ -1,171 +1,38 @@
 #include "main.h"
 
-#define P
-char *format_int(int n, char b[]);
-char *format_uns_int(unsigned int n, char b[]);
-void _putchar(char c);
-#ifdef M
-int main(void)
-{
-	print_num(-762534);
-}
-#endif
+int (*get_format_func(const char *format, int i))(const char *, va_list, int);
 
-#ifdef P
+/**
+ * _printf - a function that prints formated output to stdout
+ * @format: the argument with the string to be printed and any format specifier
+ * Return: returns the number of character printed
+ */
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0, k = 0, c = 0;
-
+	int i = 0, len = 0;
 	va_list args;
-
-	int tmp;
-
-	unsigned int utmp;
-
-	char *buf, *numStr;
-
-	char b[20];
-
-	while (format[c++]);
-
-
-	buf = malloc(sizeof(char) * (c * 2));
-
-	if (buf == NULL)
-		return (-1);
+	int (*ptr)(const char *, va_list, int);
 
 	va_start(args, format);
-
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			ptr = get_format_func(format, i);
+			if (ptr == NULL)
 			{
-				tmp = va_arg(args, int);
-
-				numStr = format_int(tmp, b);
-
-				for (j = 0; numStr[j] != '\0'; j++)
-				{
-					buf[k++] = numStr[j];
-				}
-				i += 2;
+				_putchar('%', len);
+				continue;
 			}
-			else if (format[i + 1] == 'u')
-			{
-				utmp = va_arg(args, unsigned int);
-
-				numStr = format_uns_int(utmp, b);
-
-				for (j = 0; numStr[j] != '\0'; j++)
-				{
-					buf[k++] = numStr[j];
-				}
-				i += 2;
-			}
+			len += ptr(format, args, i);
+			va_arg(args, void *);
+			i += 2;
 		}
-		buf[k++] = format[i];
+
+		len += _putchar(format[i], len);
 	}
+
 	va_end(args);
-	buf[k] = '\0';
-	c = 0;
-	i = 0;
-	while (buf[i++])
-		c++;
 
-	write(1, buf, c);
-
-	free(buf);
-	return (c);
+	return (len);
 }
-
-char *format_int(int n, char b[])
-{
-
-	int i = 0, c = 0, c1 = 0;
-	int n1 = n;
-	int isNeg = 0;
-	char tmp;
-
-	if (n < 0)
-	{
-		isNeg = 1;
-		n1 = -n;
-	}
-
-	while (1)
-	{
-		if ((n1 / 10) == 0)
-		{
-			b[i++] = (n1 + '0');
-			break;
-		}
-		b[i++] = (n1 % 10) + '0';
-		n1 = n1 / 10;
-	}
-	if (isNeg == 1)
-		b[i++] = '_';
-
-	b[i] = '\0';
-
-	i = 0;
-
-	while (b[++c]);
-
-	c1 = c - 1;
-
-	for (i = 0; i < c / 2; i++)
-	{
-		tmp = b[i];
-		b[i] = b[c1];
-		b[c1--] = tmp;
-	}
-	b[c] = '\0';
-
-	return (b);
-
-}
-
-char *format_uns_int(unsigned int n, char b[])
-{
-	unsigned int i = 0, c = 0, c1 = 0;
-	unsigned int n1 = n;
-	char tmp;
-
-	while (1)
-	{
-		if ((n1 / 10) == 0)
-		{
-			b[i++] = (n1 + '0');
-			break;
-		}
-		b[i++] = (n1 % 10) + '0';
-		n1 = n1 / 10;
-	}
-
-	b[i] = '\0';
-
-	i = 0;
-
-	while (b[++c]);
-
-	c1 = c - 1;
-
-	for (i = 0; i < c / 2; i++)
-	{
-		tmp = b[i];
-		b[i] = b[c1];
-		b[c1--] = tmp;
-	}
-	b[c] = '\0';
-
-	return (b);
-
-}
-
-void _putchar(char c)
-{
-	write(1, &c, 1);
-}
-#endif

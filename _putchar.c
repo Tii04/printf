cf147023e36aa05d36eefad101e9e768f7c7af5b
lola10209 <shanteltestimony@gmail.com>
2022-08-09 02:23:27 -0700

    Current directory

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,17 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to standard output
- * @c: the character to print
- * Return: on success 1, on error -1
+ * _putchar - prints a single character to stdout and increments len by 1
+ * @c: the character to be printed
+ * @len: will be used in the calling function to determine how many characters
+ * were printed
+ * Return: returns len (1)
  */
-int _putchar(char c)
+int _putchar(char c, int len)
 {
-	return (write(1, &c, 1));
+	len = 0;
+
+	write(1, &c, 1);
+	len++;
+	return (len);
 }

d871a1580543565c25bdcc6e3e790e26e6670982
lola10209 <shanteltestimony@gmail.com>
2022-08-09 09:34:26 -0700

    Current directory

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+
+/**
+ * _printf - A frunction that produces output according to a format
+ * @format: a character string argument passed
+ *
+ * Return: Number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int result = 0, a;
+	va_list par;
+	int (*func)(va_list);
+
+	va_start(par, format);
+	if (format == 0)
+		return (-1);
+	for (a = 0; format[a]; a++)
+	{
+		if (formal[a] == '%')
+		{
+			a++;
+			if (!(format[a]))
+				return (-1);
+			func = get_func(format[a]);
+			if (func == 0)
+			{
+				_putchar('%');
+				_putchar(format[a]);
+				result += 2;
+			}
+			else
+			{
+				result += func(par);
+			}
+		}
+		else
+		{
+			_putchar(format[a]);
+			result++;
+		}
+	}
+	va_end(par);
+	return (result);
+}
+ 
--- main.h
+++ main.h
@@ -15,5 +15,6 @@ int print_int(const char *format, va_list args, int i);
 int print_uns_int(const char *format, va_list args, int i);
 int (*get_format_func(const char *format, int i))(const char *, va_list, int);
 int prints_string(char *string);
+int print_percent(__attribute__((unused))va_list par);
 
 #endif /* MAIN_H */
